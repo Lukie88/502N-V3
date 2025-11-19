cefd6eeb5c0c94434b98cfb879384ba6a77a2037
Lukie88 <lucaslu168168@gmail.com>
2025-11-18 18:53:20 -0800

    Moved auton to auton.cpp

--- src/auton_routines.cpp
+++ src/auton_routines.cpp
@@ -12,7 +12,37 @@ namespace auton_routes {
 
 void red_1() {
     // TEMP: simple placeholder – you can replace this later
-    chassis.setPose(0, 0, 0);
+chassis.setPose(-48,15,90);
+setWingDescore(true);
+runIntakeStore();
+chassis.moveToPose(-18, 26, 70,1500,{.minSpeed=30});
+
+chassis.moveToPose(-5,48, 30,1500,{.maxSpeed =127});
+setMatchLoad(true);
+pros::delay(500);
+setMatchLoad(false);
+pros::delay(1000);
+
+chassis.moveToPose(-24, 23, 45, 1500, {.forwards=false, .minSpeed=60});
+pros::delay(500);
+
+chassis.moveToPose(-6, 9, -45, 2000,{.forwards=false});
+pros::delay(1000);
+stopIntakes();
+pros::delay(500);
+scoreMiddleGoal();
+pros::delay(2000);
+stopIntakes();
+chassis.moveToPose(-42, 47, -45, 1500,{.minSpeed=60});
+chassis.moveToPose(-70, 47, -90, 1000,{.minSpeed=50});
+setMatchLoad(true);
+runIntakeStore();
+pros::delay(2000);
+chassis.moveToPose(-10, 43, -90, 2000,{.forwards=false});
+pros::delay(500);
+setMatchLoad(false);
+pros::delay(1500);
+scoreHighGoal();
 }
 
 void red_2() {

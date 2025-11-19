100cc00e736e2c13825a45d22f9ea4aaf4db3317
Lukie88 <lucaslu168168@gmail.com>
2025-11-19 14:55:49 -0800

    -Added preset so middle goal spins opposite direction before scoring -Added initial auton for skills

--- src/main.cpp
+++ src/main.cpp
@@ -81,11 +81,18 @@ void competition_initialize() {
  * from where it left off.
  */
 void autonomous() {
-chassis.setPose(-48.813,13.767,0);
+chassis.setPose(-56,-15,180);
 setWingDescore(true);
 runIntakeStore();
-chassis.moveToPose(-48.333, 46.78, 90,1500,{.minSpeed=40});
-chassis.moveToPose(-65.159,46.139, 90,1500,{.forwards=false, .minSpeed=60});
+chassis.moveToPose(-48, -46, 180,1500,{.maxSpeed=127});
+chassis.moveToPose(-65.159,-46.139, -90,1500,{.forwards=false,.maxSpeed=127});
+chassis.moveToPose(-63, -47, -90,1200,{.maxSpeed=127});
+pros::Task::delay(200);
+setMatchLoad(true);
+runIntakeStore();
+pros::Task::delay(2000);
+
+
 
     
 
@@ -109,7 +116,6 @@ void opcontrol() {
 
 
 	while (true) {
-    clear_screen();
  int forward = controller.get_analog(pros::E_CONTROLLER_ANALOG_LEFT_Y);
 int turn    = controller.get_analog(pros::E_CONTROLLER_ANALOG_RIGHT_X);
 
